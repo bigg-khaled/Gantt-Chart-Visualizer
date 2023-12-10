f71f0e4350d51170ec6942ae3f6d82457b8dc540
bigg-khaled <khaled.sharara@gmail.com>
2023-12-11 00:19:11 +0200

    Set variables for user input

--- src/Components/OSForm.jsx
+++ src/Components/OSForm.jsx
@@ -4,46 +4,32 @@ import { useState } from "react";
 const OSForm = () => {
   const [NewItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
-  const Person = () => {
-    const [person, setPerson] = useState({
-      name: "John Doe",
-      age: 30,
-      job: "Developer",
-    });
+  const process = (arrival_time, priority, burst) => {
+    return { arrival_time: arrival_time, priority: priority, burst: burst };
   };
+  const [processes, setProcesses] = useState([]);
+
+  const [arrival_time, setArrival_time] = useState([]);
+  const [priority, setPriority] = useState([]);
+  const [burst, setBurst] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    setTodos((currentTodos) => {
+    setProcesses((currentProcesses) => {
       return [
-        ...todos,
+        ...processes,
         { id: crypto.randomUUID(), title: NewItem, completed: false },
       ];
     });
-    setNewItem("");
   }
-
-  function toggleTodo(id, completed) {
-    setTodos((currentTodos) => {
-      return currentTodos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed };
-        }
-
-        return todo;
-      });
-    });
-  }
-
-  // function deleteTodo(id)
 
   return (
     <>
       <div className="bg-slate-800 pt-5 h-screen ">
         <div className="w-1/2 flex mx-auto ">
           <form onSubmit={handleSubmit} className="new-item-form">
-            <div className="grid grid-flow-col auto-cols-max hover:auto-cols-min grid grid-cols-[200px_auto]">
+            <div className="grid-flow-col auto-cols-max hover:auto-cols-min grid grid-cols-[200px_auto]">
               <div className="bg-slate-50 ">2</div>
               <div className="bg-green-500 ">1</div>
               <div className="bg-slate-50 ">3</div>
@@ -59,29 +45,29 @@ const OSForm = () => {
                 <div class="grid grid-cols-4 gap-4">
                   <input
                     type="text"
-                    id="processes"
+                    id="arrival_time"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Arrival Time"
-                    value={NewItem}
-                    onChange={(e) => setNewItem(e.target.value)}
+                    value={arrival_time}
+                    onChange={(e) => setArrival_time(e.target.value)}
                     required
                   />
                   <input
                     type="text"
                     id="processes"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="1, 2, 3..."
-                    value={NewItem}
-                    onChange={(e) => setNewItem(e.target.value)}
+                    placeholder="Priority"
+                    value={priority}
+                    onChange={(e) => setPriority(e.target.value)}
                     required
                   />
                   <input
                     type="text"
                     id="processes"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="1, 2, 3..."
-                    value={NewItem}
-                    onChange={(e) => setNewItem(e.target.value)}
+                    placeholder="Burst Time"
+                    value={burst}
+                    onChange={(e) => setBurst(e.target.value)}
                     required
                   />
                 </div>
@@ -100,7 +86,7 @@ const OSForm = () => {
                     <label
                       type="checkbox"
                       checked={todo.completed}
-                      onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                      //   onChange={(e) => toggleTodo(todo.id, e.target.checked)}
                     >
                       {todo.title}
                     </label>
