0551641b0e7a34dba216acd4adce2431f8931d8a
bigg-khaled <khaled.sharara@gmail.com>
2023-12-13 14:55:07 +0200

    Added reset button and enter key

--- src/Components/OSForm.jsx
+++ src/Components/OSForm.jsx
@@ -38,6 +38,17 @@ const OSForm = () => {
     setNumOfProcesses(numOfProcesses + 1);
   };
 
+  const handleReset = () => {
+    setProcesses([]);
+    setNumOfProcesses(1);
+    setArrival_time("");
+    setPriority("");
+    setBurst("");
+    setAlgorithm("");
+    setButtonPressed(false);
+    setWaitingTimeArray([]);
+  };
+
   const handleWaitingTime = () => {
     let waitingTimeArrayTemp = [];
 
@@ -57,20 +68,23 @@ const OSForm = () => {
     setButtonPressed(true);
 
     if (algorithm === "FCFS") {
-      handleWaitingTime();
     } else if (algorithm === "SJF") {
       const sortedProcesses = [...processes];
       sortedProcesses.sort((a, b) => a.burst - b.burst);
       setProcesses(sortedProcesses);
-      handleWaitingTime();
       console.log("handled waiting time: ", waitingTimeArray);
     } else if (algorithm === "P-NonP") {
       const sortedProcesses = [...processes].sort(
         (a, b) => b.priority - a.priority
       );
       setProcesses(sortedProcesses);
-      handleWaitingTime();
       console.log("handled waiting time: ", waitingTimeArray);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
     }
   };
 
@@ -119,19 +133,29 @@ const OSForm = () => {
                     placeholder="Burst Time"
                     value={burst}
                     onChange={(e) => setBurst(e.target.value)}
+                    onKeyDown={handleKeyDown}
                     required
                   />
                 </div>
               </div>
             </div>
-            <div className="text-right">
+            <div className="flex justify-end">
               <button
                 type="button"
-                className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                onClick={handleSubmit}
+                class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                onClick={handleReset}
               >
-                Add
+                Reset
               </button>
+              <div className="text-right">
+                <button
+                  type="button"
+                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                  onClick={handleSubmit}
+                >
+                  Add
+                </button>
+              </div>
             </div>
 
             <div class="relative overflow-x-auto">
